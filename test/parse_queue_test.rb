cc14dd5d3686784ccd63c226f823e8466a7576b3
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-08-19 20:42:59 -0400

    Began refactoring tests.

--- test/parse_queue_test.rb
+++ test/parse_queue_test.rb
@@ -1,6 +1,16 @@
-require "test_helper"
+# coding: utf-8
+
+require_relative '../lib/parse_queue'
+gem              'minitest'
+require          'minitest/autorun'
+require          'minitest_visible'
+
 
 class ParseQueueTest < Minitest::Test
+
+  #Track mini-test progress.
+  include MinitestVisible
+
   def test_that_it_has_a_version_number
     refute_nil ::ParseQueue::VERSION
   end

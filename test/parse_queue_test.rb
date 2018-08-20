61d6d4ceb80bd75cb90930dae9b31774f01b9df0
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-08-20 13:35:37 -0400

    Reorg of tests.

--- test/parse_queue_test.rb
+++ test/parse_queue_test.rb
@@ -16,7 +16,18 @@ class ParseQueueTest < Minitest::Test
     assert(/\d*\.\d*\.\d*/ =~ ::ParseQueue::VERSION)
   end
 
-  def test_that_it_acts_like_a_queue
+  def test_that_it_acts_like_a_queue_one
+    pq = ParseQueue.new
+    pq.add(1)
+    pq.add(2)
+    pq.add(3)
+
+    assert_equal(1, pq.get)
+    assert_equal(2, pq.get)
+    assert_equal(3, pq.get)
+  end
+
+  def test_that_it_acts_like_a_queue_two
     pq = ParseQueue.new
     pq.add((1..3).to_a)
 

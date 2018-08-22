080b5c5b4280732c828d060e4874de0d9388565a
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-08-22 19:59:47 -0400

    Corrected a test regex.

--- test/parse_queue_test.rb
+++ test/parse_queue_test.rb
@@ -13,7 +13,7 @@ class ParseQueueTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil(::ParseQueue::VERSION)
     assert(::ParseQueue::VERSION.is_a?(String))
-    assert(/\d+\.\d+\.\d+/ =~ ::ParseQueue::VERSION)
+    assert(/^\d+\.\d+\.\d+/ =~ ::ParseQueue::VERSION)
   end
 
   def test_that_it_acts_like_a_queue_one
